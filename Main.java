a27e3d896cfa2c7e64cce058681cd016cc8e16d4
adenosinetp10 <adenosinetp10@gmail.com>
2023-03-09 19:07:02 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
 package FTP_Java;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args )throws Exception{
+        Scanner sc = new Scanner(System.in);
         FTP ftp = new FTP();
-        ftp.server();
+        System.out.println("1. Server, 2.Client");
+        int choice;
+        choice=sc.nextInt();
+        if (choice==1){
+            ftp.server();
+        }
+        if (choice==2){
+            ftp.client();
+        }
     }
 }
